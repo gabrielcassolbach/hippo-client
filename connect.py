425424cdbee870fecdba6e8a9d0a1781d5efa50f
gabrielcassolbach <gabrielcassolbach@gmail.com>
2024-05-31 17:58:26 -0300

    feat: skeleton of graphic-db created + store of connections

--- connect.py
+++ connect.py
@@ -1,29 +1,28 @@
 import mysql.connector
 import psycopg2
 
-def connect_to_database_psql():
+def connect_to_database_psql(connection_data):
     try:
-        conn = psycopg2.connect(
-            host="localhost",
-            user="postgres",
-            password="postgres",
-            database="template1",
-            port="5433"
+        conn2 = psycopg2.connect(
+            host = connection_data['host'],
+            user = connection_data['user'],
+            password = connection_data['password'],
+            database = connection_data['database'],
+            port = connection_data['port']
         )
-        #if conn.is_connected():
-        #    print("Conexão bem-sucedida")
-        #    return conn
+        return conn2
     except psycopg2.DatabaseError as err:
         print(f"Erro: {err}")
         return None
 
-def connect_to_database():
+def connect_to_database(connection_data):
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="password",
-            database="University"
+            host = connection_data['host'],
+            user = connection_data['user'],
+            password = connection_data['password'],
+            database = connection_data['database'],
+            port = connection_data['port']
         )
         if conn.is_connected():
             print("Conexão bem-sucedida")
@@ -34,11 +33,17 @@ def connect_to_database():
 
 def query_database(conn, query):
     cursor = conn.cursor()
+    print(cursor)
     cursor.execute(query)
     results = cursor.fetchall()
     for row in results:
         print(row)
     cursor.close()
+    
+def execute_mysql_query(conn):
+    query = input("Digite sua consulta SQL:")
+    query_database(conn, query)
+    close_connection(conn)
 
 def close_connection(conn):
     conn.close()
--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 from connect import *
-
+from graphic_engine import *
 
 def main():
-    server = input("mysql os psql")
+    while True:
+        render_menu()
+        option = int(input("Type the Option:"))
+        if option == 1: 
+            connection_data = get_data()
+            mysql_connection(connection_data)
+            break
+        if option == 2: 
+            connection_data = get_data()
+            psql_connection(connection_data)
+            break
+        if option == 3:
+            print("Leaving...")
+            break
+        if option == 4:
+            connection_data = get_data()
+            save_data(connection_data)
+            break
+        if option == 5:
+            connection_data = fetch_data()
+            if(connection_data['management-system'] == "MySQL"):
+                mysql_connection(connection_data)
+            else: 
+                psql_connection(connection_data)
+            break
 
-    conn2 = connect_to_database_psql()
+def mysql_connection(connection_data):
+    conn = connect_to_database(connection_data)           
+    if(conn):
+        execute_mysql_query(conn)
 
-    #conn = connect_to_database()
-    #if conn:
-    #    while True:
-    #        query = input("Digite sua consulta SQL(sair para encerrar o programa): ")
-    #        if query.lower() == 'sair':
-    #            break
-    #        query_database(conn, query)
-    #   close_connection(conn)
-
+def psql_connection(connection_data):
+    conn = connect_to_database(connection_data)           
+    if(conn):
+        execute_mysql_query(conn)
 
 if __name__ == "__main__":
     main()
-
-# criar as conexões com os servidores. (bibliotecas)
-
-# primeiramente -> mysql.
-
-# biblioteca "gráfica" => puxar os dados do servidor e filtrar 
-
-# pela nossa biblioteca. 
-
-# 
